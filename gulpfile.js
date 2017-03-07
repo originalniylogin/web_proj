ec31b2404ec908d8e820354830e811f904bd1118
originalniylogin <il007770@gmail.com>
2017-03-07 17:30:37 +0300

    optimised

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var spritesmith = require('gulp.spritesmith')
 
 gulp.task('sass', function() {
     return gulp.src('app/sass/**/*.sass')
@@ -22,4 +23,14 @@ gulp.task('watch', ['browser-sync', 'sass'], function() {
     gulp.watch('app/sass/**/*.sass', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('sprite', function() {
+    var spriteData = gulp.src('app/img/*.*')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.css'
+        }));
+    spriteData.img.pipe(gulp.dest('app/img/'));
+    spriteData.css.pipe(gulp.dest('app/css'));
 })
